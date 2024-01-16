5e0ef36816b37f22f14781e2a0416fe90df6815d
nothinfoyou <128641542+nothinfoyou@users.noreply.github.com>
2024-01-16 14:17:36 -0500

    js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-=======
->>>>>>> ca0bad1d8e773ee6daab994db8829732d82974bd
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -13,9 +10,4 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("header").style.marginLeft = "0";
-   
-<<<<<<< HEAD
   }
-=======
-  }
->>>>>>> ca0bad1d8e773ee6daab994db8829732d82974bd
