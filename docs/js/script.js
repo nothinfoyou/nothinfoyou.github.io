7fec6092add7b91c9269eedff48e067fbd00ad52
nothinfoyou <128641542+nothinfoyou@users.noreply.github.com>
2024-02-19 13:14:42 -0500

    modified docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -11,3 +11,18 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("header").style.marginLeft = "0";
   }
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+
+  for (i = 0; i < dropdown.length; i++){
+    dropdown[i].addEventListener("click", function(){
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display == "block"){
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
